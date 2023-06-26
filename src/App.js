1720812d78c6f6a6e956e8ec78a7cbfdea049292
kidus Tesfaye <kidus.z3rd@gmail.com>
2023-06-26 12:18:06 +0300

    'introduction_to_states'

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
 
 import './App.css';
-import { User   } from './Users';
+import { useState } from 'react';
+
 
 function App() {
 
-  
-const planets = [
-  {name:"Mars" , isGasPlanet: false },
-   {name:"Earth" ,  isGasPlanet: false },
-    {name:"Jupiter",  isGasPlanet: true },
-    {name:"Venus",  isGasPlanet: true },
-    {name:"Neptune",  isGasPlanet: true },
-    {name:"Uranus",  isGasPlanet: true }
-   
-]
 
-return(
-  planets.map((planet)=>{
-  return ( 
-    
-  planet.isGasPlanet == true ? <h2>{planet.name}</h2> : <h2></h2>
-  
-    
-    
-    
-    )
-  })
- 
-)
- 
+var [age, setAge] = useState(0);
+
+const increase = () =>{
+  setAge(age+1);
+  console.log(age);
+}
+  return <div className='App'>
+<h1>{age}</h1>
+<button onClick={increase }>Increase Age</button>
+  </div>
+
 }
 
 
